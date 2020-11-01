43ec5e2028e4c827932d635f11ab6a0d97676688
SaquibAnsari0101 <asaquib454@gmail.com>
2020-11-01 19:02:35 +0530

    Created

---
+++ app/src/main/java/com/sqb/implicit_intent/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sqb.implicit_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
